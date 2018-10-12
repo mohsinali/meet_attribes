99ed636123ce299d4cdeba4dbda47b8e2da8eba2
SMTaha <s.m.taha10@gmail.com>
2018-10-12 21:14:00 +0500

    update files to handle tslint related errors

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -14,83 +14,101 @@ const request = require("express-validator");
 export let signin = (req: Request, res: Response, next: NextFunction) => {
   let msg = {};
 
-  req.assert("email", "Email is not valid").isEmail();
-  req.assert("password", "Password cannot be blank").notEmpty();
-  req.sanitize("email").normalizeEmail({ gmail_remove_dots: false });
+  req
+    .assert("email", "Email is not valid")
+    .isEmail();
+  req
+    .assert("password", "Password cannot be blank")
+    .notEmpty();
+  req
+    .sanitize("email")
+    .normalizeEmail({gmail_remove_dots: false});
 
   const errors = req.validationErrors();
 
   if (errors) {
-    msg = { error: "Please enter a valid email and password." };
+    msg = {
+      error: "Please enter a valid email and password."
+    };
     res.json(msg);
     return;
   }
 
-  passport.authenticate(
-    "local",
-    (err: Error, user: UserModel, info: IVerifyOptions) => {
-      if (err) {
-        return next(err);
-      }
-
-      if (!user) {
-        msg = { error: "User with this email does not exist." };
-        res.json(msg);
-        return;
-      }
-      req.logIn(user, err => {
-        if (err) {
-          return res.json({
-            error: "The email and password you entered is incorrect."
-          });
-        }
-
-        res.json({ error: "Login success", token: "hello" });
-        return;
-      });
+  passport.authenticate("local", (err: Error, user: UserModel, info: IVerifyOptions) => {
+    if (err) {
+      return next(err);
     }
-  )(req, res, next);
+
+    if (!user) {
+      msg = {
+        error: "User with this email does not exist."
+      };
+      res.json(msg);
+      return;
+    }
+    req.logIn(user, err => {
+      if (err) {
+        return res.json({error: "The email and password you entered is incorrect."});
+      }
+
+      res.json({error: "Login success", token: "hello"});
+      return;
+    });
+  })(req, res, next);
 };
 
 // ########################################################################
 export let signup = (req: Request, res: Response, next: NextFunction) => {
   let msg = {};
-  req.assert("email", "Email is not valid").isEmail();
+  req
+    .assert("email", "Email is not valid")
+    .isEmail();
   req
     .assert("password", "Password must be at least 4 characters long")
-    .len({ min: 4 });
-  req.sanitize("email").normalizeEmail({ gmail_remove_dots: false });
+    .len({min: 4});
+  req
+    .sanitize("email")
+    .normalizeEmail({gmail_remove_dots: false});
 
-  const errors = req.validationErrors();
+  const errors = req.validationErrors()as MappedError[];
 
-  const err_msgs = [];
+  const err_msgs: string[] = [];
   if (errors) {
-    errors.forEach(function (e) {
-      err_msgs.push(e.msg);
-    });
-    return res.json({ error: err_msgs });
+    errors
+      .forEach(function (e) {
+        err_msgs.push(e.msg);
+      });
+    return res.json({error: err_msgs});
   }
 
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
-  });
-  User.findOne({ email: req.body.email }, (err, existingUser) => {
+  const user = new User({email: req.body.email, password: req.body.password});
+  User.findOne({
+    email: req.body.email
+  }, (err, existingUser) => {
     if (err) {
       return next(err);
     }
     if (existingUser) {
-      msg = { error: true, message: "Account with that email address already exists." };
+      msg = {
+        error: true,
+        message: "Account with that email address already exists."
+      };
       return res.json(msg);
     }
 
     user.save(err => {
       if (err) {
-        msg = { error: true, message: err };
+        msg = {
+          error: true,
+          message: err
+        };
         return res.json(msg);
       }
 
-      msg = { error: false, message: "User created successfully." };
+      msg = {
+        error: false,
+        message: "User created successfully."
+      };
       return res.json(msg);
     });
   });
--- src/passport.ts
+++ src/passport.ts
@@ -2,23 +2,18 @@ import passport from "passport";
 const LocalStrategy = require("passport-local").Strategy;
 import { default as User, UserModel, AuthToken } from "./models/User";
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password"
-    },
-    function(email: string, password: string, cb) {
-      const user = new User({ email, password });
-      return user
-        .findOne({ email, password })
-        .then(user => {
-          if (!user) {
-            return cb(null, false, { message: "Incorrect email or password." });
-          }
-          return cb(null, user, { message: "Logged In Successfully" });
-        })
-        .catch(err => cb(err));
-    }
-  )
-);
+passport.use(new LocalStrategy({
+  usernameField: "email",
+  passwordField: "password"
+}, function (email: string, password: string, cb: Function) {
+  const user = new User({email, password});
+  return (user as any)
+    .findOne({email, password})
+    .then((user: any) => {
+      if (!user) {
+        return cb(undefined, false, {message: "Incorrect email or password."});
+      }
+      return cb(undefined, user, {message: "Logged In Successfully"});
+    })
+    .catch((err: any) => cb(err));
+}));
